dbf1b200d207ee3b5b394422595e52c153c93ec6
Usmaniac2003 <usmancodepro@gmail.com>
2023-08-15 03:34:01 +0500

    React Router added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import axios from "axios";
 import { Button, Typography } from '@mui/material';
 import { MyStates } from './StateManage';
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
   let { state } = MyStates();
   return (
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { MyContextProvider } from './StateManage';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <MyContextProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </MyContextProvider>
+  <Router>
+    <MyContextProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </MyContextProvider>
+  </Router>
 );
 
